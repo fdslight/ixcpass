02a41c89a4e42f9fd1c1f47267586027e344a06f
linux <freekai@outlook.com>
2024-04-27 18:24:18 +0800

    ...

--- test.py
+++ test.py
@@ -5,5 +5,8 @@ fdst = os.popen('ip addr')
 for line in fdst:
     p = line.find("end0")
     if p < 0: continue
+    p = line.find("master")
+    if p >= 0: continue
     print(line)
+
 fdst.close()
